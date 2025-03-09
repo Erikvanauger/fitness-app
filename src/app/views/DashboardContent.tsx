e3a516c9d4adf89722c15e0131ca3a67082f87f3
Erikdenarge <Erik.workspace1@gmail.com>
2025-03-09 19:12:34 +0100

    Recipes update

--- src/app/views/DashboardContent.tsx
+++ src/app/views/DashboardContent.tsx
@@ -3,37 +3,24 @@ import Image from "next/image";
 import { useKeenSlider } from "keen-slider/react";
 import "keen-slider/keen-slider.min.css";
 import { ArrowRight, Star, MessageSquareHeart, Sun } from "lucide-react";
-import recipeData from '../data/recipes.json';
+import recipeData from "../data/recipes.json";
 
 function DashboardContent() {
   const slides = [
     {
       src: "/Food.jpg",
       alt: "Slide 1",
-      text: [
-        "The trick to a",
-        "Tasty",
-        "Green",
-        "Plate",
-      ],
+      text: ["The trick to a", "Tasty", "Green", "Plate"],
     },
     {
       src: "/Run.jpg",
       alt: "Slide 2",
-      text: [
-        "Take your first step",
-        "to an active",
-        "LIFESTYLE",
-      ],
+      text: ["Take your first step", "to an active", "LIFESTYLE"],
     },
     {
       src: "/Yolk.jpg",
       alt: "Slide 3",
-      text: [
-        "5 secret powers",
-        "in the",
-        "YOLK",
-      ],
+      text: ["5 secret powers", "in the", "YOLK"],
     },
   ];
 
@@ -60,11 +47,19 @@ function DashboardContent() {
     return () => clearInterval(interval);
   }, [instanceRef]);
 
+  //New recipe per day
+  
+  const todayIndex = new Date().getDate() % recipeData.length;
+  const todaysRecipe = recipeData[todayIndex];
+
   return (
     <div className="grid grid-cols-4 grid-rows-8 gap-4 h-full px-4">
       {/* Top box */}
       <div className="col-span-4 row-span-1  flex items-center justify-between">
-        <h2 className="flex text-4xl font-bebas ">Good morning, Anton!<Sun className="ml-2 text-yellow-500"/></h2>
+        <h2 className="flex text-4xl font-bebas ">
+          Good morning, Anton!
+          <Sun className="ml-2 text-yellow-500" />
+        </h2>
         <button className="bg-black mr-4 py-4 px-8 rounded-full text-white">
           Upgrade
         </button>
@@ -109,22 +104,34 @@ function DashboardContent() {
       </div>
 
       {/* Box 2 */}
-      <div className="col-span-1 row-span-7 bg-red-400 rounded-lg p-4">
-        <h2 className="text-xl font-bold">Todays Recipe Tip</h2>
-        {recipeData.map(recipe => (
-          <div key={recipe.id} className="mb-4 cursor-pointer">
-            <h3 className="text-lg font-semibold">{recipe.title}</h3>
-            <p>{recipe.description}</p>
-            <button className="mt-2 bg-white text-black rounded py-1 px-3">
-              View Recipe
-            </button>
+      <div className="col-span-1 row-span-7 bg-red-400 rounded-lg px-4 py-2">
+        <h2 className="text-xl font-bold">Todays Recipe</h2>
+        <div className="mb-4 ">
+          <h3 className="text-[2rem] font-medium lilita-one-regular">
+            {todaysRecipe.title}
+          </h3>
+          <div className="flex justify-center items-center">
+            <Image
+              src={todaysRecipe.image}
+              alt={todaysRecipe.title}
+              width={320}
+              height={200}
+              className="object-cover rounded-lg"
+            />
           </div>
-        ))}
+          <p>{todaysRecipe.description}</p>
+          <button
+            className="mt-2 bg-white text-black rounded py-1 px-3 cursor-pointer"
+            
+          >
+            View Recipe
+          </button>
+        </div>
       </div>
 
       {/* Box 3 */}
       <div className="bg-white rounded-lg row-span-3 p-4 flex flex-col gap-y-4">
-        {/* Review-stjärnor */}
+        {/* Review-stars */}
         <div className="flex gap-2 ">
           {[...Array(4)].map((_, i) => (
             <Star key={i} className="text-yellow-500 fill-yellow-500 w-8 h-8" />
@@ -136,8 +143,8 @@ function DashboardContent() {
           <h4 className="text-xl font-bold ">Try our new Running Tracker!</h4>
         </div>
         <p className="text-sm mt-2">
-          Track your runs, set goals, and analyze your progress with ease. <br /> Stay
-          motivated and improve your performance every day!
+          Track your runs, set goals, and analyze your progress with ease.{" "}
+          <br /> Stay motivated and improve your performance every day!
         </p>
 
         <div className="flex justify-between items-center mt-auto">
